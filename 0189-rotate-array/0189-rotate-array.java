230801138316a1109cac464bdadcc800dafa8e1a
Rohan Maiti <132077931+rohanmaiti@users.noreply.github.com>
2024-12-27 23:19:57 +0530

    Time: 16 ms (5.09%), Space: 55.3 MB (98.45%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.java
@@ -0,0 +1,23 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int size = nums.length;
+        int K;
+        if(size > k){
+            K = k;
+        }
+        else{
+            K = k%size; // no of element to be pickedup to temp
+        }
+        ArrayList<Integer> l = new ArrayList<>();
+        for(int i=size-K;i<size;i++){
+            l.add(nums[i]);
+        }
+        for(int index = size-K-1;index>=0;index--) {
+            nums[index+K] = nums[index];
+        }
+        System.out.println(l);
+        for(int i=0;i<l.size();i++){
+            nums[i] = l.get(i);
+        }
+    }
+}
