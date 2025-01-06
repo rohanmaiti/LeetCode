5a9541c4d4b8485d781382dea22ea6c5e7063de4
Rohan Maiti <maitirohan2019@gmail.com>
2025-01-06 14:25:19 +0530

    Time: 1 ms (99.65%), Space: 55.4 MB (68.82%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,35 +1,49 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int leftMultiple = 1;
-        int rightMultiple = 1;
-        int zeroCount = 0;
-        int sum = 0;
-        for(int i=0;i<nums.length;i++){
-         rightMultiple *= nums[i];
-         if(nums[i] == 0) zeroCount++;
-        }
-        int result[] = new int[nums.length];
-        for(int i=0;i<nums.length;i++){
-            if(nums[i] == 0){
-                zeroCount--;
-                if(zeroCount == 0){
-                    rightMultiple = 1;
-                    for(int j=i+1;j<nums.length;j++){
-                        rightMultiple *= nums[j];
-                    }
-                  //  System.out.print("rightMultiple =" + rightMultiple);
-                }
-                else{
-                    rightMultiple = 0;
-                }
-            }
-            else{
-                rightMultiple /= nums[i];
-            }
-            int ans = rightMultiple * leftMultiple;
-            result[i] = ans;
-            leftMultiple *= nums[i];
+        // int leftMultiple = 1;
+        // int rightMultiple = 1;
+        // int zeroCount = 0;
+        // int sum = 0;
+        // for(int i=0;i<nums.length;i++){
+        //  rightMultiple *= nums[i];
+        //  if(nums[i] == 0) zeroCount++;
+        // }
+        // int result[] = new int[nums.length];
+        // for(int i=0;i<nums.length;i++){
+        //     if(nums[i] == 0){
+        //         zeroCount--;
+        //         if(zeroCount == 0){
+        //             rightMultiple = 1;
+        //             for(int j=i+1;j<nums.length;j++){
+        //                 rightMultiple *= nums[j];
+        //             }
+        //         }
+        //         else{
+        //             rightMultiple = 0;
+        //         }
+        //     }
+        //     else{
+        //         rightMultiple /= nums[i];
+        //     }
+        //     int ans = rightMultiple * leftMultiple;
+        //     result[i] = ans;
+        //     leftMultiple *= nums[i];
 
+        // }
+        // return result;
+
+        // code-02
+        int result[] = new int[nums.length];
+        for(int i=0;i<nums.length;i++);
+        int prefix = 1;
+        for(int i=0;i<nums.length;i++){
+            result[i] = prefix;
+            prefix *= nums[i];
+        }
+        int postfix = 1;
+        for(int i=nums.length-1;i>=0;i--){
+            result[i] *= postfix;
+            postfix *= nums[i];
         }
         return result;
     }
