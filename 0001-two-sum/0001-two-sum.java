be9585c438c4d8a7714642812bd3e448449b115a
Rohan Maiti <maitirohan2019@gmail.com>
2025-01-17 09:55:29 +0530

    Time: 2 ms (98.94%), Space: 45 MB (57.69%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,15 +1,21 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+        // brute agaya O(n^2)
+        // [2, 7, 11, 15]
+        // better using hashing 
         HashMap<Integer, Integer> m = new HashMap<>();
         for(int i=0;i<nums.length;i++){
-            int x = target - nums[i];
-            if(m.containsKey(x)){
-                return new int[]{m.get(x),i};
+            int a = nums[i];
+            int remaining = target-a;
+            if(m.containsKey(remaining)){
+                // means other number that will complete the target is present
+                int j = m.get(remaining); 
+                return new int[]{j,i};
             }
-            else{
-                m.put(nums[i],i);
-            }
-        }
-        return new int[]{};
+            // writing here to deal with  duplicate elements
+            m.put(nums[i],i);
+        }  
+        return new int[]{-1,-1};
+
     }
 }
