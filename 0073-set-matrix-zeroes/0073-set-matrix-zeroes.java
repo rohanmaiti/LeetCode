2be6f0a96cf882ba6c4cff3e75dcc4ba42657587
Rohan Maiti <maitirohan2019@gmail.com>
2025-02-13 19:18:09 +0530

    Time: 1 ms (74.88%), Space: 45.7 MB (65.53%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,24 +1,51 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
+        // APPROACH - 01
+        // int m = matrix.length;
+        // int n = matrix[0].length;
+        // HashSet<Integer> rowSet = new HashSet<>();
+        // HashSet<Integer> colSet = new HashSet<>();
+        // for(int i=0;i<m;i++){
+        //     for(int j=0;j<n;j++){
+        //         if(matrix[i][j] == 0){
+        //             rowSet.add(i);
+        //             colSet.add(j);
+        //         }
+        //     }
+        // }
+        // for(var val : rowSet){
+        //     for(int i=0;i<n;i++)
+        //     matrix[val][i] = 0;
+        // }
+        //  for(var val : colSet){
+        //     for(int i=0;i<m;i++)
+        //     matrix[i][val] = 0;
+        // }
+
+        // APPROACH - 02
         int m = matrix.length;
         int n = matrix[0].length;
-        HashSet<Integer> rowSet = new HashSet<>();
-        HashSet<Integer> colSet = new HashSet<>();
+        int row[] = new int[m];
+        int col[] = new int[n];
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(matrix[i][j] == 0){
-                    rowSet.add(i);
-                    colSet.add(j);
-                }
+               if(matrix[i][j] == 0){
+                  row[i] = 1;
+                  col[j] = 1;
+               }
             }
         }
-        for(var val : rowSet){
-            for(int i=0;i<n;i++)
-            matrix[val][i] = 0;
+         for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+               if(row[i] == 1 || col[j] == 1){
+                 matrix[i][j] = 0;
+               }
+            }
         }
-         for(var val : colSet){
-            for(int i=0;i<m;i++)
-            matrix[i][val] = 0;
-        }
+        return;
+
+        
+
     }
+  
 }
