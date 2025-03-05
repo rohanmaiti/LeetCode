99dd66b8e35e0c4ab53be8776eb5181330198481
Rohan Maiti <maitirohan2019@gmail.com>
2025-03-05 22:12:11 +0530

    Time: 0 ms (100%), Space: 42.9 MB (29.99%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,21 +1,18 @@
 class Solution {
-    public void sol(int []nums, int i, int n, List<Integer> current, List<List<Integer>> result){
-        if(i == n){
-            result.add(new ArrayList<>(current));
+    public void sol(int nums[], List<Integer> l, int idx, List<List<Integer>> result){
+        if(idx == nums.length){
+            result.add(new ArrayList<>(l));
             return;
         }
-        // include
-        current.add(nums[i]);
-        sol(nums, i+1, n, current, result);
-        // exclude
-        current.remove(current.size()-1);
-        sol(nums, i+1, n, current, result);
-        return;
+        l.add(nums[idx]);
+        sol(nums, l, idx+1, result);
+        l.remove(l.size()-1);
+        sol(nums, l, idx+1, result);
     }
     public List<List<Integer>> subsets(int[] nums) {
+        List<Integer> l = new ArrayList<>();
         List<List<Integer>> result = new ArrayList<>();
-        List<Integer> list = new ArrayList<>();
-        sol(nums, 0, nums.length, list, result);
+        sol(nums, l, 0, result);
         return result;
     }
 }
