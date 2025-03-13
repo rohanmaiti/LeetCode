3e9ece65b74edcb2874d5f21b5753a54686ec833
Rohan Maiti <maitirohan2019@gmail.com>
2025-03-13 12:48:12 +0530

    Time: 36 ms (21.49%), Space: 45.3 MB (18.61%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -9,16 +9,16 @@ class Solution {
         while(j < arr.length){
            m.put(arr[j],m.getOrDefault(arr[j],0)+1);
            maxFreq = Math.max(maxFreq, m.get(arr[j]));
-           while(((j-i+1 )- maxFreq) > k){
+           if(((j-i+1 )- maxFreq) > k){
             maxFreq = 0;
             m.put(arr[i],m.get(arr[i])-1);
             if(m.get(arr[i]) == 0){
                 m.remove(arr[i]);
             }
-            for(var key : m.keySet()){
-                if(m.get(key) > maxFreq)
-                maxFreq = m.get(key);
-            }
+            // for(var key : m.keySet()){
+            //     if(m.get(key) > maxFreq)
+            //     maxFreq = m.get(key);
+            // }
             i++;
            }
            ans = Math.max(ans, j-i+1);
